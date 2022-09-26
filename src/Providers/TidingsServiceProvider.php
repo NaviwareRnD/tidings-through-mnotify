9d31710a1c524bd5dfd01b6497c66840057e6951
KwesiNavilot <andrewskwesiankomahene@gmail.com>
2022-09-26 21:40:42 +0000

    Removed mergeConfigFrom from Provider

--- src/Providers/TidingsServiceProvider.php
+++ src/Providers/TidingsServiceProvider.php
@@ -12,12 +12,6 @@
         $this->app->bind('tidings', function ($app) {
             return new Tidings();
         });
-
-        /**
-         * Access the config values from the config helper
-         * by prefixing our "key" as follows: config('tidings.mnotify_api_key').
-         */
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'tidings');
     }
 
     public function boot()
